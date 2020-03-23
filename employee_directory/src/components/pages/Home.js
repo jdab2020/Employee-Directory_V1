ddb9306553987bb521227f040c56a78e0bd92ca9
jdab2020 <jdabalos90@gmail.com>
2020-03-23 00:01:50 -0700

    added all components | need to debug home | need to add forms

---
+++ employee_directory/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InfoCard from "../InfoCard"
+import employeesData from "../../employee.js"
+
+function Home() {
+    const employee = employeesData.map(employee =>
+        <InfoCard key={employee.id} employee={employee} />)
+
+    return (
+        <div style={{ backgroundColor: "teal" }} className="row">
+            {employee}
+        </div>
+    )
+}
+
+export default Home;
